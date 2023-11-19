d66cea069caeba9c1a76c7cd44add4d3fbe0a084
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-11-19 18:55:56 +0530

    new

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,56 +1,29 @@
 import React from 'react'
-import "./hero.css"
-import {HiLocationMarker} from "react-icons/hi"
-// import {CountUp} from 'react-countup/build/CountUp'
 
 const Hero = () => {
   return (
     <section className='hero-wrapper'>
-        <div className="paddings innerWidth flexCneter hero-container">
-            <div className="flexColStart  hero-left">
+        <div className="paddings innerWidth flexCenter hero-container">
+            <div className="hero-left">
                 <div className="hero-title">
-                    <div className="orange-circle">
-                       <h1>Discover <br /> Luxury Villas</h1>
-
-                    </div>
+                    <h1>Discover <br />beautiful Villas</h1>
                 </div>
-                <div className="flexColStart hero-des">
-                    <span>Find a home that reflects your aspirations</span> <br />
-                    <span>K.B. Developers offering you a great home <br /> with state of the art construction <br />, architecture and amenities</span>
-                </div>
-                <div className="flexCenter search-bar">
-                    <HiLocationMarker color="var--blue)" size={25}/>
-                    <input type="text" />
-                    <button className='button'>Search</button>
+                <div className="hero-des">
+                    <span>Find a variety of properties that reflect your aspirations</span>
+                    <span>Forget your difficulties in finding residence for you</span>
                 </div>
 
-                <div className="flexCenter stats">
-                    <div className="flexColStart stat">
-                        <span>
-                            50
-                            <span>+</span>
-                        </span>
-                        <span className='secondaryText'>Premium Villas</span>
-                    </div>
-
-                    <div className="flexColStart stat">
-                        <span>
-                            200
-                            <span>+</span>
-                        </span>
-                        <span className='secondaryText'>Happy Families</span>
-                    </div>
-                </div>
+                <div className="search-bar"></div>
             </div>
+              {/* right side */}
             <div className="flexCenter hero-right">
                 <div className="image-container">
-                    <img src="https://greenlakes.in/wp-content/uploads/2023/08/DSC04531-HDR-1-1-1024x683.jpg" alt="" />
+                    <img src="./hero-image.png" alt="" />
                 </div>
             </div>
         </div>
     </section>
-
-    )
+  )
 }
 
 export default Hero
