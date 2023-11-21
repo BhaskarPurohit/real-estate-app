fa56d1b0ddd520c01da6b52af9f745d703fae2a9
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-11-21 13:17:09 +0530

    slider sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import "./App.css"
 import Companies from "./components/companies/Companies";
+import Residency from "./components/residencies/Residency";
 function App() {
   return (
 <div className="App">
@@ -12,6 +13,7 @@ function App() {
     <Hero/>
   </div>
   <Companies/>
+  <Residency/>
 </div>  
 
 
