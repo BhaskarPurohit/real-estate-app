a7c21c47aa5ec6f61ad71d3e249a3db6f6b7f1b3
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-11-22 13:46:19 +0530

    values

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -1,74 +1,78 @@
 import React, { useState } from 'react'
 import {
     Accordion,
-    AccordionItemButton,
-    AccordionItemHeading,
     AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
     AccordionItemPanel,
     AccordionItemState
-
-
 } from "react-accessible-accordion"
 
 import "react-accessible-accordion/dist/fancy-example.css"
-import { MdOutlineArrowDropDown } from "react-icons/md"
+import {MdOutlineArrowDropDown} from "react-icons/md"
+import './value.css'
 import data from "../../utils/accordion"
 
 const Value = () => {
-    return (
-        <section className="v-wrapper">
-            <div className="paddings innerWidth flexCenter v-container">
-                {/* left side */}
-                <div className="v-left">
-                    <div className="image-container">
-                        <img src="./value.png" alt="" />
-
-                    </div>
+    const [className, setClassName] = useState(null)
+  return (
+    <section className="v-wrapper">
+        <div className="paddings innerWidth flexCenter v-container">
+            {/* left section */}
+            <div className="v-left">
+                <div className="image-container">
+                    <img src="./value.png" alt="" />
                 </div>
-
-                {/* {right side} */}
-
-                <div className="flexColStart v-right">
-                    <span className='orangeText'>Our values</span>
-                    <span className='primaryText'>Values we give to you</span>
-                    <span className='secondaryText'>
-                        Always ready for good customer service <br />
-                        we believe goof gkdnjvnfo
-                    </span>
-                    <Accordion className='accordion' allowMultipleExpanded={false}
-                        preExpanded={[0]}>
-                        {
-                            data.map((item, i) => {
-                                // const [className, setClassName] = useState(null)
-                                return (
-                                    <AccordionItem className='accordionItem' key={i} uuid={i}>
-                                        <AccordionItemHeading>
-                                            <AccordionItemButton>
-                                                <div className="flexCenter icon">{item.icon}</div>
-                                                <span className="primaryText">
-                                                    {item.heading}
-                                                </span>
-                                                <div className="flexCenter icon">
-                                                    <MdOutlineArrowDropDown size={20}/>
-                                                </div>
-                                            </AccordionItemButton>
-                                        </AccordionItemHeading>
-                                    </AccordionItem>
-                             
-                                )
-                            })
-                        }
-
-                    </Accordion>
-
-
-                </div>
-
-
             </div>
+            {/* right side */}
+            <div className="flexColStart v-right">
+                <span className='orangeText'>Our Values</span>
+                <span className='primaryText'>Values we give to you</span>
+                <span className='secondaryText'>
+                    Lorem ipsum dolor, sit amet <br />
+                    consectetur adipisicing elit. Nisi eius mollitia reiciendis har
+                </span>
 
-        </section>
-    )
+                <Accordion
+                className='accordion'
+                allowMultipleExpanded={false}
+                preExpanded={[0]}
+                >
+                    {
+                        data.map((item, i )=>{
+                            return(
+                                <AccordionItem className={`accordionTiem ${className}`} key={i} uuid={i}>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton className='flexCenter accordionButton'>
+                                            <AccordionItemState>
+                                                {({expanded}) => expanded ? setClassName("expanded") : setClassName("collapsed")}
+                                            </AccordionItemState>
+                                            <div className="flexCenter icon">
+                                                {item.icon}
+                                            </div>
+                                            <span className="primaryText">
+                                                {item.heading}
+                                            </span>
+                                            <div className="flexCenter icon">
+                                                <MdOutlineArrowDropDown size={20}/>
+                                            </div>
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p className="secondaryText">
+                                            {item.detail}
+                                        </p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+                            )
+                        })
+                    }
+
+                </Accordion>
+            </div>
+        </div>
+    </section>
+  )
 }
 
 export default Value
