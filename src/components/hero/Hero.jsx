f82be7442f6dc9968bb214a920295dfdd9b4fb73
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-11-23 11:23:47 +0530

    footer

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./hero.css"
 import CountUp from 'react-countup'
+import {animate, motion} from "framer-motion"
 
 const Hero =()=> {
   return (
@@ -10,9 +11,14 @@ const Hero =()=> {
             <div className="flexColStart hero-left">
                 <div className="hero-title">
                     <div className="orange-circle"/>
-                    <h1>
+                    <motion.h1 initial={{y:"2rem"}}
+                    animate={{y:0, opacity:1}}
+                    transition={{
+                        duration:2,
+                        type:"spring"
+                    }}>
                         Discover <br /> new villas property 
-                    </h1>
+                    </motion.h1>
                 </div>
                 <div className="flexColStart hero-des">
                     <span className='secondaryText'>Find a vairety of properties that suit your style</span>
@@ -48,9 +54,12 @@ const Hero =()=> {
             </div>
                 {/* right side  */}
             <div className="flexCenter hero-right">
-                <div className="image-container">
+                <motion.div initial={{x:"7rem", opacity:0}} animate={{x:0, opacity:1}} transition={{
+                    duration:2,
+                    type:"spring"
+                }} className="image-container">
                     <img src="./hero-image.png" alt="" />
-                </div>
+                </motion.div>
 
             </div>
 
