79b984120fb0f60b7a3f5d2539f0a35a2123603c
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-26 20:06:40 +0530

    Co-authored-by: Satyam Mishra <satyam-mishra-pce@users.noreply.github.com>

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -17,17 +17,14 @@ const Contact = () => {
                 {/* first row  */}
                 <div className="flexStart row">
                     <div className="flexColCenter mode">
-                        <div className="flexStart">
-                            <div className="flexCenter icon">
+                        <div className="flexStart ">
+                            <div className="flexCenter  icon">
                                 <MdCall size={25}/>
                             </div>
                             <div className="flexColStart detail">
                                 <span className='primaryText'>Call</span>
                                 <span className='secondaryText'></span>
                             </div>
-                        </div>
-                        <div className="flexCenter button">
-                        <a href="tel:8107117767">Call Now</a>
                         </div>
                     </div>
                     {/* second mode  */}
@@ -43,6 +40,20 @@ const Contact = () => {
                     </div>
                 </div>
 
+                <div className="flexColCenter mode">
+                        <div className="flexStart">
+                            <div className="flexCenter icon">
+                                <BsFillChatDotsFill size={25}/>
+                            </div>
+                            <div className="flexColStart detail">
+                                <span className='primaryText'>Book a Visit</span>
+                                <span className='secondaryText'></span>
+                            </div>
+                        </div>
+                        <div className="flexCenter button">
+                            Send Details 
+                        </div>
+                    </div>
                 {/* second row  */}
                 <div className="flexStart row">
                     <div className="flexColCenter mode">
@@ -80,8 +91,8 @@ const Contact = () => {
             </div>
             {/* right side */}
             <div className="c-right">
-                <div className="image-container">
-                    <img src="photos/contact.png" alt="" />
+                <div className="map-container">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3539.741360321972!2d76.72414727620185!3d27.4773103355984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDI4JzM4LjMiTiA3NsKwNDMnMzYuMiJF!5e0!3m2!1sen!2sin!4v1703599788528!5m2!1sen!2sin" width="800" height="600" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,32 +2,45 @@ import React from 'react'
 import "./footer.css"
 const Footer = () => {
   return (
-    <section className="f-wrapper">
-        <div className="paddings innerWidth flexCenter f-container">
-            {/* left-side  */}
-            <div className="flexColStart f-left">
-                <img src="photos/logo2.png" width={120} alt="" />
-                <span className="secondaryText">
-                    Lorem ipsum dolor sit amet, <br /> consectetur adipisicing.
-
-
-
-                </span>
+    <section className='footer' style={{backgroundImage: `url("/photos/bg-img-footer.png")`}}>
+        <div className='dimmer'>
+            <div className="left">
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</span>
             </div>
-            {/* right side */}
-            <div className="flexColStart f-right">
-                <span className='primaryText'>Information</span>
-                <span className='secondaryText'>151 civil lines jaipur</span>
-
-                <div className="flexCenter f-menu">
-                    <span>property</span>
-                    <span>property</span>
-                    <span>property</span>
-                    <span>property</span>
-                </div>
+            <div className="right">
+                <div className="title">Information</div>
+                <div className="subtitle">151 Civil Lines Jaipur</div>
+                <div className="content"></div>
             </div>
         </div>
     </section>
+    
+//     <section className="f-wrapper" style={{background: `url("/photos/bg-img-footer.png") center no-repeat 200% 200%`}}>
+//     <div className="paddings innerWidth flexCenter f-container">
+//         {/* left-side  */}
+//         <div className="flexColStart f-left">
+//             {/* <img src="photos/bg-img-footer.png" width={120} alt="" /> */}
+//             <span className="secondaryText">
+//                 Lorem ipsum dolor sit amet, <br /> consectetur adipisicing.
+
+
+
+//             </span>
+//         </div>
+//         {/* right side */}
+//         <div className="flexColStart f-right">
+//             <span className='primaryText'>Information</span>
+//             <span className='secondaryText'>151 civil lines jaipur</span>
+
+//             <div className="flexCenter f-menu">
+//                 <span>property</span>
+//                 <span>property</span>
+//                 <span>property</span>
+//                 <span>property</span>
+//             </div>
+//         </div>
+//     </div>
+// </section>
   )
 }
 
